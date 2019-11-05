6c2aa647e89ad3d87c10111d1e063baa3a1597d1
HN-Vignolles <31932468+HN-Vignolles@users.noreply.github.com>
2019-11-05 13:21:40 -0300

    Create max7456.c

---
+++ max7456.c
@@ -0,0 +1,15 @@
+#include main.h
+#include max7456.h
+
+extern struct spi_ioc_transfer xfer
+
+void writeAddrData(uint8_t addr, uint8_t data){
+  uint8_t buf[2];
+  buf[0] = addr;
+  buf[1] = data;
+  xfer.tx_buf = (unsigned long)buf;
+  xfer.len = 2;
+  if((ioctl(fd, SPI_IOC_MESSAGE(1), &xfer)) < 0){
+    perror("SPI_IOC_MESSAGE");
+  }
+}
