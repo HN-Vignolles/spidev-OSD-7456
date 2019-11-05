f7beea27a2d763716e5a26717244492ecdc63cc3
HN-Vignolles <31932468+HN-Vignolles@users.noreply.github.com>
2019-11-05 11:57:37 -0300

    Update osdctl.c

--- osdctl.c
+++ osdctl.c
@@ -9,10 +9,12 @@
 #include <linux/spi/spidev.h>
 
 typedef struct{
+  char filename[PATH_MAX];
   int fd;
-  uint8_t mode, lsb, bits;
+  uint8_t mode, bits;
   uint32_t speed=5000000;
 }spi_t;
+spi_t initspi;
 
 int spidev_init(void);
 
@@ -23,3 +25,35 @@ int main(int argc, char **argv){
     return EXIT_FAILURE;
   }
   while((c = getopt(argc,argv,"f:")) != EOF){
+    switch(c){
+      case 'f':
+        sprintf(initspi.filename,"%s",optarg);
+        break;
+      case '?':
+        fprintf(stderr,"unrecognized option: -%c",c);
+        return EXIT_FAILURE;
+    }
+  }
+  initspi.mode = SPI_MODE_0;
+  /*The SPI commands are 16 bits long with the 8 most significant bits (MSBs) representing the register address
+    and the 8 least significant bits (LSBs) representing the data*/
+  initspi.bits = 8; 
+  spidev_init();
+}
+
+int spidev_init(void){
+  //int ret;
+  if((initspi.fd = open(initspi.filename,O_RDWR)) < 0){
+    perror("%s",initspi.filename);
+    return EXIT_FAILURE;
+  }
+  if((ioctl(initspi.fd, SPI_IOC_WR_MODE, &mode)) < 0){
+    perror("ioctl");
+    return EXIT_FAILURE;
+  }
+  if((ioctl(initspi.fd, SPI_IOC_RD_MODE, &mode)) < 0){
+    perror("ioctl");
+    return EXIT_FAILURE;
+  }
+  
+}
